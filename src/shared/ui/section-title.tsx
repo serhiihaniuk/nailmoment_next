734f158087fa8ca635450144d2109f2084ff2fff
Zamiliukhin Valentyn <valentynzamiliukhin@gmail.com>
2024-08-08 23:15:05 +0200

    Finish with previous festivals section for mobile

--- src/shared/ui/section-title.tsx
+++ src/shared/ui/section-title.tsx
@@ -1,6 +1,13 @@
-export const SectionTitle = ({ children }: { children: string }) => {
+import {cn} from "@/shared/utils/cn";
+
+export const SectionTitle = ({ children, className }: { children: string, className?: string }) => {
   return (
-    <h2 className="text-xl text-primary-foreground w-min text-center uppercase font-travels font-bold block mx-auto">
+    <h2
+      className={cn(
+        "text-xl text-primary-foreground w-min text-center uppercase font-travels font-bold block mx-auto",
+        className
+      )}
+    >
       {children}
     </h2>
   );
